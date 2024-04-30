24a45d063656968e60546338b8b0b3683ebad6be
Shafaq Mughal <160257641+shafaqmughal20@users.noreply.github.com>
2024-04-30 09:02:40 +0500

    Add files via upload

---
+++ Q-22.ts
@@ -0,0 +1,11 @@
+//22.Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+//creating array
+let errorArray : string[] =[
+    "Hamza","usama","sadaf"];
+
+// creating error by accessing invalid index of array
+//console.log(errorArray[4]);
+
+//removing error
+console.log(errorArray[2]);
